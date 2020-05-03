83626fa3d92eac2520cb7faf9bb43cac77ae1949
Andy Trofimov <webgrandmo@gmail.com>
2020-05-03 20:32:31 +0300

    Create 7-level-Friend-or-Foe.js

---
+++ 7-level-Friend-or-Foe.js
@@ -0,0 +1,11 @@
+const friend = friends => {
+  //your code here
+  //Loop through array
+  //Convert item each item to string
+  //Check the length  each item
+  //Return array with items 4 letter long
+  
+  return friends.filter( item => {
+    return item.toString().length === 4;
+  });
+}
