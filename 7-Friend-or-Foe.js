32883bfadfa88110da83c1d46a4a82326eb3774a
Andy Trofimov <webgrandmo@gmail.com>
2020-05-03 20:41:24 +0300

    Update 7-Friend-or-Foe.js

--- 7-Friend-or-Foe.js
+++ 7-Friend-or-Foe.js
@@ -1,13 +1,9 @@
 const friend = friends => {
   //your code here
   //Loop through array
-  //Convert item each item to string
   //Check the length  each item
   //Return array with items 4 letter long
-  
-  return friends.filter( item => {
-    return item.toString().length === 4;
-  });
+   return friends.filter( item => item.length === 4);
 }
 
 friend(["Ryan", "Kieran", "Mark"]);
